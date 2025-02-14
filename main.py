126b8ec9e40f32cf0e0ad1838988a455eb6e1498
Mouez Yazidi <56160970+Mouez-Yazidi@users.noreply.github.com>
2025-02-14 11:24:23 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from markitdown import MarkItDown
+
+def resume_parsing(uploaded_file):
+  md = MarkItDown() 
+  result = md.convert("/content/1736075776301.pdf")
+  return result.text_content
+  
+
+def main():
+    st.title("File Uploader and Text Zone")
+    
+    uploaded_file = st.file_uploader("Upload a PDF or DOCX file", type=["pdf", "docx"])
+    text_input = st.text_area("Enter additional text here:")
+    
+    if uploaded_file is not None:
+        extracted_text = resume_parsing(uploaded_file)
+        
+        st.subheader("Extracted Text:")
+        st.text_area("", extracted_text, height=300)
+    
+    if text_input:
+        st.subheader("Entered Text:")
+        st.write(text_input)
+
+if __name__ == "__main__":
+    main()
