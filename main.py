e3634a0725bfaf9ed528b26defba5424103d7de8
Mouez Yazidi <56160970+Mouez-Yazidi@users.noreply.github.com>
2025-02-18 09:54:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,25 @@ def resume_parsing(uploaded_file):
   
 
 def main():
-    st.set_page_config(
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Run the Streamlit app.')
+    parser.add_argument('--environment', 
+                        type=str, 
+                        choices=['local', 'cloud'], 
+                        default='cloud',
+                        help='Specify the environment: "local" or "cloud".')
+    args = parser.parse_args()
+    
+    if args.environment == 'cloud':
+        # Access secret values
+        groq_key = st.secrets["GROQ_KEY"]
+    else:
+        from dotenv import load_dotenv
+        load_dotenv()
+        # Access secret values
+        groq_key = os.getenv("GROQ_KEY")
+    
+  st.set_page_config(
         layout="wide"  # This makes the app take the full page width
     )
     
@@ -23,8 +41,10 @@ def main():
     if uploaded_file is not None and job_description:
       if st.button('Analyze'):
           parsed_resume = resume_parsing(uploaded_file)
-          swot_analysis = swot_analyzer(parsed_resume, job_description)
-          html_content = html_tamplate(swot_analysis)
+          swot_analysis = swot_analyzer(api_key = groq_key,
+                                        parsed_resume = parsed_resume, 
+                                        job_description = job_description)
+          html_content = html_tamplate(swot_analysis = swot_analysis)
           st.components.v1.html(f"""{html_content}""", width=1000, height=1000)
 if __name__ == "__main__":
     main()
